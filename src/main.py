1dfca6f5f3c368c51b058aec4723ee198f4c9c8b
Kelli Scheuble <github@scheuble.dev>
2020-07-28 11:48:33 -0700

    Basic Byglet scaffolding

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
+import pyglet
+
 from board import Board
 
 def main():
-	pass
+	window = pyglet.window.Window()
+	label = pyglet.text.Label('Hello, world',
+	                          font_name='Times New Roman',
+	                          font_size=36,
+	                          x=window.width//2, y=window.height//2,
+	                          anchor_x='center', anchor_y='center')
+
+	@window.event
+	def on_draw():
+	    window.clear()
+	    label.draw()
+
+	pyglet.app.run()
 
 
 if __name__ == '__main__': 
