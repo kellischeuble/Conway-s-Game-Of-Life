040cbe618d5c669ccc65ca4c3a455074caca4be3
Kelli Scheuble <github@scheuble.dev>
2020-07-28 18:04:51 -0700

    IDK what's different with this tbh

--- src/main.py
+++ src/main.py
@@ -3,7 +3,12 @@ import pyglet
 from board import Board
 
 def main():
-	window = pyglet.window.Window()
+	# set up game window
+	window = pyglet.window.Window(
+		width=800,
+		height=800,
+		caption="Conway's Game of Life"
+	)
 	label = pyglet.text.Label('Hello, world',
 	                          font_name='Times New Roman',
 	                          font_size=36,
@@ -15,8 +20,11 @@ def main():
 	    window.clear()
 	    label.draw()
 
-	pyglet.app.run()
+	@window.event
+	def on_key_press(symbol, modifiers):
+		print(f"the {symbol} key was pressed")
 
 
 if __name__ == '__main__': 
-	main() 
+	window = Window()
+	pyglett.app.run()
